4f652d6e5ce8d092de8a0c38fa47cbb398e42cc4
JeanPierreSantMarc <j.pierre@santmarc.com>
2021-06-02 14:37:50 +0200

    MenuInicial

---
+++ Assets/Scripts/Menumanager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class Menumanager : MonoBehaviour
+{
+   
+    public GameObject CanvasOpciones;
+    public bool opciones;
+    Canvas canvas;
+
+    void Start()
+    {
+        canvas = CanvasOpciones.GetComponent<Canvas>();
+        canvas.enabled = false;
+    }
+
+    //Opciones************************
+    public void Opciones()
+    {
+        opciones = !opciones;
+        canvas.enabled = opciones;
+        Time.timeScale = (opciones) ? 0 : 1f;
+    }
+
+    //Jugar************************
+    public void SceneChanger(int sceneId)
+    {
+        SceneManager.LoadScene(sceneId);
+    }
+
+    //Salir************************
+    public void CloseGame()
+    {
+        Application.Quit();
+    }
+
+    //Volumen************************
+    public AudioMixer audioMixer;
+    public void SetVolume (float volume)
+    {
+        audioMixer.SetFloat("volume", volume);
+    }
+
+   
+}
